820a4784f86fca96d57274d0b4d330593641058b
Joao Lino <linusmannuel@gmail.com>
2024-08-02 11:26:18 +0100

    aprenda a criar um formulario no flutter

--- lib/telas/autenticacao_tela.dart
+++ lib/telas/autenticacao_tela.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gymapp_live/_comum/minhas_cores.dart';
 
-class AutenticacaoTela extends StatelessWidget {
+class AutenticacaoTela extends StatefulWidget {
   const AutenticacaoTela({super.key});
 
+  @override
+  State<AutenticacaoTela> createState() => _AutenticacaoTelaState();
+}
+
+class _AutenticacaoTelaState extends State<AutenticacaoTela> {
+  bool queroEntrar = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,21 +28,69 @@ class AutenticacaoTela extends StatelessWidget {
               ])
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset('assets/logo.png', height: 128),
-              const Text(
-                "GymApp",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset('assets/logo.png', height: 128),
+                      const Text(
+                        "GymApp",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 32.0,),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("Email"),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("Senha"),
+                        ),
+                        obscureText: true,
+                      ),
+                      Visibility(
+                        visible: !queroEntrar,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Confirme Senha")
+                              ),
+                              obscureText: true,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Nome"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 32.0,),
+                      ElevatedButton(onPressed: (){}, child: Text((queroEntrar)? "Entrar" : "Cadastrar")),
+                      const Divider(),
+                      TextButton(onPressed: (){
+                        setState(() {
+                          queroEntrar = !queroEntrar;
+                        });
+                      }, 
+                      child: Text((queroEntrar)?"Ainda não tem uma conta? Cadastra-se!" : 'Já tem uma conta? Entrar'))
+                    ],
+                  ),
                 ),
-              )
-            ],
+              ),
+            ),
           ),
         ],
       ),
